6d664a9a9cb9c2a2557c0a730f071e5dcebd2506
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-10-06 05:26:38 +0200

    Update utils.py

--- bnet/utils.py
+++ bnet/utils.py
@@ -1,25 +1,18 @@
-import os
+from os import environ
 import requests
 
-from bnet.exceptions import BattleNetError
+from .exceptions import BattleNetError
 
 AUTH_URL = "https://us.battle.net/oauth/token"
 
-
 def generate_access_token(client_id=None, secret=None):
-    if not all([client_id, secret]):
+    if client_id is None and secret is None:
         try:
-            client_id = os.environ["BATTLE_NET_CLIENT_ID"]
-            secret = os.environ["BATTLE_NET_SECRET"]
+            client_id = environ["BATTLE_NET_CLIENT_ID"]
+            secret = environ["BATTLE_NET_SECRET"]
         except KeyError:
-            raise ValueError(
-                "Either provide client_id and secret or set environment variables BATTLE_NET_CLIENT_ID, BATTLE_NET_SECRET"
-            )
-
-    response = requests.post(
-        AUTH_URL, auth=(client_id, secret), data={"grant_type": "client_credentials"}
-    ).json()
+            raise ValueError("Either provide client_id and secret or set environment variables BATTLE_NET_CLIENT_ID, BATTLE_NET_SECRET")
+    response = requests.post(AUTH_URL, auth=(client_id, secret), data={"grant_type": "client_credentials"}).json()
     if "error" in response:
         raise BattleNetError(response)
-    else:
-        return response
+    return response
