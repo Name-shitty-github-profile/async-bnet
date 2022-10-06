80120d19e6a64038a311dc3199b1b0575363924b
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-10-06 05:19:06 +0200

    Update client.py

--- bnet/client.py
+++ bnet/client.py
@@ -1,30 +1,23 @@
-from bnet.exceptions import IncorrectMethod
+from .exceptions import IncorrectMethod
 
 
 class BattleNetClient(object):
-
     def __init__(self, connection):
         self.connection = connection
-
         self._method = None
         self._endpoint = None
 
     def __getattr__(self, endpoint):
-        if '_' in endpoint:
-            endpoint = endpoint.split('_')
-            self._endpoint = '/'.join(endpoint[1:])
-            self._method = endpoint[0].upper()
-
-        else:
-            raise IncorrectMethod(endpoint)
-
+        if not "_" in endpoint: raise IncorrectMethod(endpoint)
+        endpoint = endpoint.split('_')
+        self._endpoint = '/'.join(endpoint[1:])
+        self._method = endpoint[0].upper()
         return self
 
     def __call__(self, *args, **kwargs):
-        endpoint_arguments = '/'.join(args)
         return self.connection._make_request(
             method=self._method,
             endpoint=self._endpoint,
-            endpoint_arguments=endpoint_arguments,
+            endpoint_arguments='/'.join(args),
             parameters=kwargs
         )
